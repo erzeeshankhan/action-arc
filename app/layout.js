4e3388bb704b413c0894afe8c2c1e446aa070bb3
erzeeshankhan <er.zeeshank@gmail.com>
2025-01-18 17:15:30 +0530

    Cler API Installation

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,8 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
 import { Inter } from 'next/font/google';
-import  Header  from '@/components/header';
+import Header from '@/components/header';
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,23 +13,26 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <head>
-        <meta name="description" content={metadata.description} />
-      </head>
-      <body className={`${inter.className}`}>
-        <ThemeProvider attribute="class" defaultTheme="dark">
-          <Header />
-          <main className="min-h-screen">
-            {children}
-          </main>
-          <footer className="bg-gray-900 py-10">
-            <div className="container mx-auto px-4 text-center text-gray-200">
-              <p>Made By Zeeshan Khan </p>
-            </div>
-          </footer>
-        </ThemeProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+
+      <html lang="en">
+        <head>
+          <meta name="description" content={metadata.description} />
+        </head>
+        <body className={`${inter.className}`}>
+          <ThemeProvider attribute="class" defaultTheme="dark">
+            <Header />
+            <main className="min-h-screen">
+              {children}
+            </main>
+            <footer className="bg-gray-900 py-10">
+              <div className="container mx-auto px-4 text-center text-gray-200">
+                <p>Made By Zeeshan Khan </p>
+              </div>
+            </footer>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- components/header.jsx
+++ components/header.jsx
@@ -1,8 +1,18 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const header = () => {
   return (
-    <div>header</div>
+    <div>
+    {/* If the user is signed out - showing signIn button */}
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      {/* If the user is signed in - showing the "User" button */}
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </div>
   )
 }
 
