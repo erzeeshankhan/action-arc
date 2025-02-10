54880f8122a028d163e0e877502836e87b25673e
erzeeshankhan <er.zeeshank@gmail.com>
2025-02-10 19:16:08 +0530

    adding auth to sprint manager and sprints action

--- action/sprints.js
+++ action/sprints.js
@@ -37,3 +37,51 @@ export async function createSprint(projectId, data) {
     return sprint;
 
 }
+
+
+export async function updateSprintStatus(sprintId, newStatus) {
+    // auth checks
+    const { userId, orgId, orgRole } = auth();
+    if (!userId || !orgId) {
+        throw new Error('User is not authenticated');
+    }
+
+    try {
+        const sprint = await db.sprint.findUnique({
+            where: { id: sprintId },
+            include: { project: true },
+        });
+
+        if (!sprint || sprint.project.organizationId !== orgId) {
+            throw new Error('Sprint not found');
+        }
+        if (orgRole !== "org:admin") {
+            throw new Error('Only Admins can make this change');
+        }
+        if (orgRole === "org:member") {
+            throw new Error('You are not authorized to make this change');
+        }
+        const now = new Date();
+        const startDate = new Date(sprint.startDate);
+        const endDate = new Date(sprint.endDate);
+
+        // if sprint is ended and user is trying to make changes to it
+        if (newStatus === "ACTIVE" && (now < startDate || now > endDate)) {
+            throw new Error('Cannot start sprint outside of sprint date range');
+        }
+        // if user is trying to complete the sprint and sprint is not active
+        if(newStatus === "COMPLETED" && sprint.status !== "ACTIVE"){
+            throw new Error('Can only end an active sprint');
+        }
+
+        const updatedSprint = await db.sprint.update({
+            where: { id: sprintId },
+            data: { status: newStatus },
+
+        });
+
+        return {success: true, updatedSprint};
+    } catch (error) {
+        throw new Error(error.message);
+    }
+}
--- app/(main)/project/components/sprint-manager.jsx
+++ app/(main)/project/components/sprint-manager.jsx
@@ -1,5 +1,8 @@
 "use client";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { format, formatDistanceToNow, isAfter, isBefore } from "date-fns";
 import { useState } from "react";
 
 import React from 'react'
@@ -19,21 +22,63 @@ const SprintManager = ({ sprint, setSprint, sprints, projectId }) => {
 
     // to decide if the sprint can be ended right now or not
     const canEnd = status === "ACTIVE";
+
+    // handle the sprint change 
+    const handleSprintChange = (value) => {
+        // finding the selected sprint in the sprint list 
+        const selectedSprint = sprints.find((s) => s.id === value);
+
+        // setting the selected sprint and its status
+        setSprint(selectedSprint);
+        setStatus(selectedSprint.status);
+    }
+
+    // function to determine if the sprint is active or not (if sprint is started or planned or ended)
+    const getStatusText = () => {
+        if (status === "COMPLETED") {
+            return `Sprint Ended`;
+        }
+        if (status === "ACTIVE" || isAfter(now, endDate)) {
+            return `Overdue by ${formatDistanceToNow(endDate, "d")}`;
+        }
+        if (status === "PLANNED" || isBefore(now, startDate)) {
+            return `Starts in ${formatDistanceToNow(startDate, "d")}`;
+        }
+        return null;
+
+    }
+
     return (
         <>
-            <div>
-                <Select>
-                    <SelectTrigger className="w-[180px]">
-                        <SelectValue placeholder="Theme" />
+            <div className="flex justify-between items-center gap-4">
+                <Select value={sprint.id} onValueChange={handleSprintChange}>
+                    <SelectTrigger className="bg-slate-900 self-start">
+                        <SelectValue placeholder="Select Sprint " />
                     </SelectTrigger>
                     <SelectContent>
-                        <SelectItem value="light">Light</SelectItem>
-                        <SelectItem value="dark">Dark</SelectItem>
-                        <SelectItem value="system">System</SelectItem>
+                        {sprints.map((sprint) => {
+                            return <SelectItem key={sprint.id} value={sprint.id}>
+                                {sprint.name} ({format(sprint.startDate, "MMM d, yyy")}) to{" "}
+                                {format(sprint.endDate, "MMM d, yyy")}
+                            </SelectItem>
+                        })}
                     </SelectContent>
                 </Select>
 
+                {/* rendering the buttons for start and ending of teh sprints */}
+                {canStart && (
+                    <Button className='bg-green-900 text-white'>Start Sprint</Button>
+                )}
+
+                {/* can end btn will show only if the sprint is started */}
+                {canEnd && (
+                    <Button className='destructive'>End Sprint</Button>
+                )}
+
             </div>
+            {getStatusText() && (
+                <Badge className='mt-3  self-start'>{getStatusText()}</Badge>
+            )}
         </>
     )
 }
